bd3a6dfea56593930291077024eec63fe49d28a5
Misbah Anwar <127953074+misbah-anwar@users.noreply.github.com>
2023-10-05 15:15:00 +0530

    Add files via upload

---
+++ lib/Strings.dart
@@ -0,0 +1,4 @@
+const String timeUp = "Time's up!";
+const String wordAlreadyUsed = "Word already used!";
+const String noValidWordFound = "No valid word found";
+const String notEnglish = "Not an English word!";
