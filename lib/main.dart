f7fa73a23fe73f504b5ff54d7e9fb5415af1cf00
Misbah Anwar <127953074+misbah-anwar@users.noreply.github.com>
2023-10-05 15:14:28 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart' as english_words;
 import 'dart:async';
+import 'dart:math';
+import 'Strings.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +26,7 @@ class MyApp extends StatelessWidget {
           toolbarHeight: 100,
           flexibleSpace: Container(
             color: Colors.white,
-            padding: const EdgeInsets.only(top: 60.0),
+            padding: const EdgeInsets.only(top: 50.0),
             child: Text(
               'Word Game',
               style: style.copyWith(fontSize: 40.0),
@@ -52,12 +54,93 @@ class _GameScreenState extends State<GameScreen> {
   String message = "";
   int timer = 60;
   late Timer _timer;
+  bool isEnded = false;
+  String start = "Start Game";
+  String end = "End Game";
+  bool _showWordsList = false;
+  late FocusNode _focusNode;
+
+  void _toggleWordsList() {
+    setState(() {
+      _showWordsList = !_showWordsList;
+    });
+  }
+
+  void _toggleGameButtonLabel() {
+    setState(() {
+      if (isEnded) {
+        start = "Start Game";
+      } else {
+        start = "End Game";
+      }
+    });
+  }
+
+  void startGame() {
+    setState(() {
+      words.clear();
+      currentWord = "";
+      message = "";
+      timer = 60;
+      isEnded = false;
+      startTimer();
+      generateComputerWord();
+      _toggleWordsList();
+      _toggleGameButtonLabel();
+      resetTimer();
+      _focusNode.requestFocus();
+    });
+  }
+
+  void endGame() {
+    _timer.cancel();
+    setState(() {
+      isEnded = true;
+      _toggleWordsList();
+      _toggleGameButtonLabel();
+      _focusNode.unfocus(); // Close the keypad
+    });
+  }
+
+  Widget buildWordsList() {
+    if (_showWordsList && isEnded) {
+      return Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Text(
+              words.join(', '),
+              style: const TextStyle(
+                fontSize: 20,
+                fontFamily: "Poppins",
+                color: Colors.blue,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+        ],
+      );
+    }
+    return Container();
+  }
 
   @override
   void initState() {
     super.initState();
     startTimer();
     generateComputerWord();
+    _focusNode = FocusNode();
+  }
+
+  void toggleGame() {
+    setState(() {
+      isEnded = !isEnded;
+      if (!isEnded) {
+        startGame();
+      } else {
+        endGame();
+      }
+    });
   }
 
   void startTimer() {
@@ -67,7 +150,7 @@ class _GameScreenState extends State<GameScreen> {
           this.timer--;
         } else {
           _timer.cancel();
-          showMessage("Time's up!");
+          showMessage(timeUp);
         }
       });
     });
@@ -89,12 +172,12 @@ class _GameScreenState extends State<GameScreen> {
     String newWord = controller.text.toLowerCase();
     if (newWord.isNotEmpty) {
       if (words.contains(newWord)) {
-        showMessage("Word already used!");
+        showMessage(wordAlreadyUsed);
       } else if (currentWord.isNotEmpty &&
           currentWord[currentWord.length - 1] != newWord[0]) {
-        showMessage("Invalid word!");
+        showMessage(noValidWordFound);
       } else if (!english_words.all.contains(newWord)) {
-        showMessage("Not an English word!");
+        showMessage(notEnglish);
       } else {
         words.add(newWord);
         currentWord = newWord;
@@ -109,7 +192,7 @@ class _GameScreenState extends State<GameScreen> {
   void generateComputerWord() {
     String lastChar = currentWord.isNotEmpty
         ? currentWord[currentWord.length - 1]
-        : english_words.all[0][0];
+        : english_words.all[Random().nextInt(english_words.all.length)][0];
 
     String computerWord = english_words.all.firstWhere(
         (word) =>
@@ -139,13 +222,12 @@ class _GameScreenState extends State<GameScreen> {
       children: [
         Padding(
           padding: const EdgeInsets.only(
-              left: 30.0, right: 30.0, top: 50.0, bottom: 10.0),
+              left: 30.0, right: 30.0, top: 20.0, bottom: 10.0),
           child: TextField(
             enabled: false,
             style: style,
-            //controller: controller,
             decoration: InputDecoration(
-                labelText: "Computer's Word: $currentWord",
+                labelText: "$currentWord",
                 border: const OutlineInputBorder(),
                 filled: true,
                 fillColor: Colors.grey[200],
@@ -154,8 +236,9 @@ class _GameScreenState extends State<GameScreen> {
         ),
         Padding(
           padding: const EdgeInsets.only(
-              left: 30.0, right: 30.0, top: 30.0, bottom: 60.0),
+              left: 30.0, right: 30.0, top: 20.0, bottom: 60.0),
           child: TextField(
+            focusNode: _focusNode, // Assign the focus node here
             style: style,
             enabled: true,
             controller: controller,
@@ -168,27 +251,54 @@ class _GameScreenState extends State<GameScreen> {
                 contentPadding: EdgeInsets.all(10.0)),
           ),
         ),
-        ElevatedButton(
-          onPressed: checkWord,
-          style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.green,
-              padding: const EdgeInsets.only(
-                  left: 50.0, right: 50.0, top: 10.0, bottom: 10.0)),
-          child: const Text(
-            'Submit',
-            style: TextStyle(
-                fontSize: 20,
-                fontFamily: "Poppins",
-                color: Colors.black54,
-                fontWeight: FontWeight.w500),
-          ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            ElevatedButton(
+              onPressed: checkWord,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                padding: const EdgeInsets.only(
+                    left: 20.0, right: 20.0, top: 10, bottom: 10),
+              ),
+              child: const Text(
+                'Submit',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: "Poppins",
+                  color: Colors.black54,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: toggleGame,
+              style: ElevatedButton.styleFrom(
+                backgroundColor: isEnded ? Colors.lightBlue : Colors.red,
+                padding: const EdgeInsets.only(
+                    left: 10.0, right: 10.0, top: 10.0, bottom: 10.0),
+              ),
+              child: Text(
+                isEnded ? start : end,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontFamily: "Poppins",
+                  color: Colors.black54,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ],
         ),
         Padding(
           padding: const EdgeInsets.only(top: 20.0, bottom: 10.0),
           child: Text(
             message,
             style: const TextStyle(
-                color: Colors.red, fontSize: 16, fontFamily: "Poppins"),
+              color: Colors.red,
+              fontSize: 16,
+              fontFamily: "Poppins",
+            ),
           ),
         ),
         Padding(
@@ -202,6 +312,7 @@ class _GameScreenState extends State<GameScreen> {
                 fontWeight: FontWeight.w500),
           ),
         ),
+        buildWordsList(),
       ],
     );
   }
